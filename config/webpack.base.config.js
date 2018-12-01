691e0fb955c6677cfe1c619af0771e8eae104e14
John Paul Veneracion <jpveneracion@gmail.com>
2018-12-01 22:57:44 +0800

    Initial Commit

---
+++ config/webpack.base.config.js
@@ -0,0 +1,59 @@
+var HTMLWebpackPlugin = require("html-webpack-plugin");
+
+var MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              minimize: true
+            }
+          },
+          {
+            loader: "sass-loader"
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].scss",
+      chunkFilename: "[id].scss"
+    })
+  ]
+};
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import styles from "./index.scss";
+
+const App = () => {
+  return (
+    <div>
+      <p>Wantastik</p>
+      <p>- Captain Kirk</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("app"));
